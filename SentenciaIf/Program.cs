723dda170e0d0149af5f049c9d4be03e7f9c7e12
Angel Montes <angelmontes963@gmail.com>
2024-05-30 21:44:52 -0600

    Sentencias If

---
+++ SentenciaIf/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SentenciaIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool tienesHambre = true;
+            bool tienesDinero = true;
+
+            if (tienesHambre && tienesDinero && EstaAbiertoRestaurante("Loches pepe", 9))
+            {
+                Console.WriteLine("Come");
+            }
+            else
+            {
+                Console.WriteLine("No comas");
+            }
+        }
+
+        static bool EstaAbiertoRestaurante(string nombre,int hora = 0)
+        {
+            if(nombre == "Loches pepe" && hora > 8 && hora < 23)
+            {
+                return true;
+            }
+            else if(nombre == "Restaurant 24 horas")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
