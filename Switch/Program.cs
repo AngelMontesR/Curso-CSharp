f8dd9e30505907447c122c995a39566722e0109f
Angel Montes <angelmontes963@gmail.com>
2024-06-05 20:47:58 -0600

    Se crea sección de switch

---
+++ Switch/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 1;
+
+            switch (opcion)
+            {
+                   case 1:
+                    Console.WriteLine("Opcion 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Opcion 2");
+                    break;
+                case 3:
+                    Console.WriteLine("Opcion 3");
+                    break;
+                case < 0:
+                case > 3:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+                default:
+                    Console.WriteLine("Opcion no valida");
+                    break;
+            }
+        }
+    }
+}
