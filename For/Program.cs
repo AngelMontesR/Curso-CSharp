ed31f64f3faa047cd096d57735f4ce2e5c4183bb
Angel Montes <angelmontes963@gmail.com>
2024-06-05 21:37:19 -0600

    For

---
+++ For/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace For
+{
+    class For
+    {
+        static void Main(string[] args)
+        {
+            string[] amigos = new string[3] {
+                "Juan",
+                "Pedro",
+                "Pablo"
+            };
+                
+            bool corre = true;
+            for (int i = 0; i < amigos.Length && corre; i++)
+            {
+                Console.WriteLine(amigos[i]);
+            }
+        }
+    }
+}
