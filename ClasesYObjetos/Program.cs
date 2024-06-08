ee1549939398a2d2cf47772af4ae22c9aa35c43d
Angel Montes <angelmontes963@gmail.com>
2024-06-08 12:47:07 -0600

    Clases y Objetos

---
+++ ClasesYObjetos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClasesYObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Sale sale = new Sale(10,DateTime.Now);
+           Console.WriteLine(sale.GetInfo());
+        }
+    }
+
+    class Sale
+    {
+        int total;
+        DateTime fecha;
+
+        public Sale(int total,DateTime fecha)
+        {
+            this.total = total;
+            this.fecha = fecha;
+        }
+
+        public string GetInfo()
+        {
+            return "Total: " + total + " Fecha: " + fecha.ToLongTimeString();
+        }
+        
+       public void Show()
+       {
+           Console.WriteLine("Hola soy una venta ");
+       }
+    }
+}
