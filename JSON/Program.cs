8ac79d14106a7f9c242cf64ad1eb6bdb581bea88
Angel Montes <angelmontes963@gmail.com>
2024-06-19 20:42:48 -0600

    JSON

---
+++ JSON/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerveza cerveza = new Cerveza
+            {
+                Marca = "Corona",
+                Tipo = "Clara",
+                Grados = 4.5
+            };
+
+            //La diagonal invertida se usa para escapar las comillas dobles
+            string json = "["+
+                    "{"+
+                        "\"Marca\":\"Corona\","+
+                        "\"Tipo\":\"Clara\","+
+                        "\"Grados\":1.2"+
+                    "}"+
+                    "{"+
+                        "\"Marca\":\"Modelo\","+
+                        "\"Tipo\":\"Negra\","+
+                        "\"Grados\":1.4"+
+                    "}"+
+                "]";
+
+            Console.WriteLine(json);
+        }
+    }
+
+    class Cerveza
+    {
+        public string Marca { get; set; }
+        public string Tipo { get; set; }
+        public double Grados { get; set; }
+    }
+}
