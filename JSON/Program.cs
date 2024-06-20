30eb3848cdd44e832c1b8355c5b951c9d159eaa5
Angel Montes <angelmontes963@gmail.com>
2024-06-19 20:53:44 -0600

    Serializar JSON

--- JSON/Program.cs
+++ JSON/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json;
 
 namespace JSON
 {
@@ -27,7 +28,15 @@ namespace JSON
                     "}"+
                 "]";
 
-            Console.WriteLine(json);
+            //Serializar un objeto a JSON , es decir convertir un objeto a un string en formato JSON
+            string json2 = JsonSerializer.Serialize(cerveza);
+
+            Console.WriteLine(json2);
+
+            //Deserializar un JSON a un objeto, es decir convertir un string en formato JSON a un objeto,Dinamic es un tipo de dato que puede ser cualquier tipo de dato
+            Cerveza cervecilla = JsonSerializer.Deserialize<Cerveza>(json2);
+
+            Console.WriteLine(cervecilla);
         }
     }
 
