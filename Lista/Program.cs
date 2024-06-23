fbbf7c773e5b06a25b1c3408a7056538d32662d7
Angel Montes <angelmontes963@gmail.com>
2024-06-23 17:27:18 -0600

    Listas

---
+++ Lista/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lista
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(1);
+            numbers.Add(2);
+
+            Console.WriteLine("Numeros 1: "+numbers.Count);
+
+            List<int> numbers2 = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            Console.WriteLine(numbers2.Count);
+
+            numbers.Clear();
+
+            Console.WriteLine("Numeros 1: " + numbers.Count);
+
+            List<string> ciudades = new List<string>()
+            {
+                "CDMX", "Guadalajara", "Monterrey"
+            };
+
+            ciudades.Add("Puebla");
+
+            Console.WriteLine("Ciudades: "+ ciudades.Count);
+        }
+    }
+}
