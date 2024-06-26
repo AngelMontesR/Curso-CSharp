1d61729066278bebf1d2e1a060cf59c272a445de
Angel Montes <angelmontes963@gmail.com>
2024-06-25 20:27:11 -0600

    Tipos Anonimos

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TiposAnonimos
+{
+    class Empezar
+    {
+        static void Main(string[] args)
+        {
+
+            //Objeto anonimo , solo se puede leer , no se puede modificar
+            var angel = new { 
+                Name = "Angel",
+                Ciudad = "Mexico"
+            };
+
+            Console.WriteLine(angel.Name);
+            Console.WriteLine(angel.Ciudad);
+
+            var gente = new[]
+            {
+                new {Nombre = "Juan"},
+                new {Nombre = "Pepe"}
+            };
+
+            foreach (var item in gente)
+            {
+                Console.WriteLine($"Nombre: {item.Nombre}");
+            }
+        }
+    }
+}
