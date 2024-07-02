e727f6632e9e571f10be1f399c89729a7e95d2d6
Angel Montes <angelmontes963@gmail.com>
2024-07-01 21:02:25 -0600

    Excepciones

---
+++ Excepciones/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Excepciones
+{
+    class Ejecutar
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string contenidoURL = File.ReadAllText(@"C:\Users\Bioxor\x\pato.txt");
+                Console.WriteLine(contenidoURL);
+
+                throw new Exception("Ocurrio algo raro");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("No existe el archivo");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Sirve para cerrar excepciones, se ejecuta siempre, aunque no haya exceptions
+                Console.WriteLine("Aqui me ejecuto pase lo que pase");
+            }
+            Console.WriteLine("Se sigue ejecutando");
+        }
+    }
+}
