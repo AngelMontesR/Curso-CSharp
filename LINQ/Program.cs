8e81a926c3e2c9fcdd24948978ad9b58606ca078
Angel Montes <angelmontes963@gmail.com>
2024-07-09 21:06:17 -0600

    LINQ

---
+++ LINQ/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Start()
+    {
+        public static void Main(string[] args)
+        {
+            List<Cerveza> cervezas = new List<Cerveza>()
+            {
+                new Cerveza()
+                {
+                    Nombre =  "Clara",
+                    Marca = "Corona"
+                },
+                new Cerveza()
+                {
+                    Nombre = "Oscura",
+                    Marca = "Victoria"
+                },
+                new Cerveza()
+                {
+                    Nombre = "Angelito",
+                    Marca = "XX"
+                }
+            };
+
+            foreach (var item in cervezas)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.WriteLine("---------------------");
+
+            //Select - Selecciona ciertos elementos de la lista
+            var beersName = from listaCerveza in cervezas
+                            select new
+                            {
+                                Nombre = listaCerveza.Nombre,
+                                Marca = listaCerveza.Marca,
+                                LongitudNombre = listaCerveza.Nombre.Length
+                            };
+
+            foreach (var item in beersName)
+            {
+                Console.WriteLine($"{item.Nombre} {item.LongitudNombre}");
+            }
+
+
+            //Filtrado - Filtra informacion de una lista como una tipo consulta
+            Console.WriteLine("---------------------");
+            var cervezaCorona = from listaCerveza in cervezas
+                                where listaCerveza.Marca == "Victoria"
+                                || listaCerveza.Marca == "Corona"
+                                select listaCerveza;
+
+            foreach (var corona in cervezaCorona)
+            {
+                Console.WriteLine(corona);
+            }
+
+
+            //Ordenar - informacion de una lista como una tipo consult
+            Console.WriteLine("---------------------");
+            var ordenaCerveza = from listaCerveza in cervezas
+                                orderby listaCerveza.Marca descending
+                                select listaCerveza;
+
+            foreach (var item in ordenaCerveza)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+
+    public class Cerveza
+    {
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Nombre} Marca: {Marca}";
+        }
+    }
+
+}
