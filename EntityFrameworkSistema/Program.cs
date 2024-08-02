fcfc03a3c2d3da6c4513fc542940cd169948a6dd
Angel Montes <angelmontes963@gmail.com>
2024-08-01 21:22:50 -0600

    Se hace mostrar información con EntityFramework

--- EntityFrameworkSistema/Program.cs
+++ EntityFrameworkSistema/Program.cs
@@ -2,6 +2,7 @@
 using BD;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace EntityFrameworkSistema
 {
@@ -12,14 +13,63 @@ namespace EntityFrameworkSistema
             DbContextOptionsBuilder<AutosDbContext> db = new DbContextOptionsBuilder<AutosDbContext>();
 
             db.UseSqlServer("Server=ANGEL; Database=AutosDB; User=montes; Password=a; TrustServerCertificate=True;");
-
-            using(AutosDbContext context = new AutosDbContext(db.Options))
+          
+            bool repetir = false;
+            int opcion = 0;
+            do
             {
-                var autos = context.Autos.ToList();
 
-                foreach (var item in autos)
+                Console.WriteLine("----------- Elige la opción ------------");
+                Menu();
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
                 {
-                    Console.WriteLine(item.Anio);
+                    case 1:
+                        Mostrar(db);
+                        break;
+                    case 2:
+                       
+                        break;
+                    case 3:
+                       
+                        break;
+                    case 4:
+                       
+                        break;
+                    case 5:
+                        repetir = false;
+                        break;
+                }
+            } while (repetir);
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("\n ------------- Menu ------------");
+            Console.WriteLine("1 Mostrar");
+            Console.WriteLine("2 Agregar");
+            Console.WriteLine("3 Editar");
+            Console.WriteLine("4 Eliminar");
+            Console.WriteLine("5 Salir");
+        }
+
+        public static void Mostrar(DbContextOptionsBuilder<AutosDbContext> conexion)
+        {
+            Console.Clear();
+            Console.WriteLine("Carros en la BD");
+
+            using (AutosDbContext context = new AutosDbContext(conexion.Options))
+            {
+                List<Auto> autos = (from aut in context.Autos
+                                    //where aut.ModeloId == 1
+                                    orderby aut.Precio descending
+                                    select aut).Include(aut => aut.Modelo).ToList();
+
+                foreach (Auto auto in autos)
+                {
+                    Console.WriteLine($"Auto {auto.Modelo.Nombre} Precio: {auto.Precio}");
                 }
             }
         }
